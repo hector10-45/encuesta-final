4303659157d83fd9e9baf0d2272df60c1d0ea78c
hector10-45 <hminota@estudiante.uniajc.edu.com>
2026-05-21 22:10:57 -0500

    fix: ruta post limpia

--- backend/routes/users.routes.js
+++ backend/routes/users.routes.js
@@ -1,18 +1,18 @@
 import express from 'express';
-// Un solo import para todas las funciones del controlador
 import { crearEncuesta, obtenerReporteIndicadores } from '../controllers/usuario.controller.js';
-
+ 
 const router = express.Router();
-
-// Ruta para guardar la encuesta (POST)
-router.post('/encuesta', crearEncuesta);
-
+ 
+// Ruta para guardar la encuesta (POST) - ¡Cámbiala a '/' para que reciba directo!
+router.post('/', crearEncuesta);
+ 
 // Ruta para obtener el reporte de mejor/peor (GET)
 router.get('/reporte', obtenerReporteIndicadores);
-
+ 
 // Ruta de prueba
 router.get('/test', (req, res) => {
     res.send('¡La ruta de usuarios funciona en el puerto 4000!');
 });
-
+ 
 export default router;
+ 
